f52d22b9dcd5b082e1191d8475ff648324132226
silverjoda <heizen.sh@gmail.com>
2020-12-03 14:08:37 +0100

    M

--- buggy/src/draft.py
+++ buggy/src/draft.py
@@ -1,43 +1,43 @@
-import numpy as np
-import quaternion as q
-import math as m
+def calculate_stabilization_action(self, orientation, angular_velocities, targets):
+    roll, pitch, _ = p.getEulerFromQuaternion(orientation)
+    roll_vel, pitch_vel, yaw_vel = angular_velocities
+    t_throttle, t_roll, t_pitch, t_yaw_vel = targets
 
-def q2e(quat):
-    w, x, y, z = quat
-    pitch = -m.asin(2.0 * (x * z - w * y))
-    roll = (
-            m.atan2(2.0 * (w * x + y * z), w * w - x * x - y * y + z * z)
-    )
-    yaw = (
-            m.atan2(2.0 * (w * z + x * y), w * w + x * x - y * y - z * z)
-    )
+    # Increase t_yaw_vel because it's slow as shit
+    t_yaw_vel *= 5
 
-    return roll, pitch, yaw
+    # print(f"Throttle_target: {t_throttle}, Roll_target: {t_roll}, Pitch_target: {t_pitch}, Yaw_vel_target: {t_yaw_vel}")
+    # print(f"Roll: {roll}, Pitch: {pitch}, Yaw_vel: {yaw_vel}")
 
+    # Target errors
+    e_roll = t_roll - roll
+    e_pitch = t_pitch - pitch
+    e_yaw = t_yaw_vel - yaw_vel
 
-def e2q(roll, pitch, yaw):
-    qx = np.sin(roll / 2) * np.cos(pitch / 2) * np.cos(yaw / 2) - np.cos(roll / 2) * np.sin(pitch / 2) * np.sin(
-        yaw / 2)
-    qy = np.cos(roll / 2) * np.sin(pitch / 2) * np.cos(yaw / 2) + np.sin(roll / 2) * np.cos(pitch / 2) * np.sin(
-        yaw / 2)
-    qz = np.cos(roll / 2) * np.cos(pitch / 2) * np.sin(yaw / 2) - np.sin(roll / 2) * np.sin(pitch / 2) * np.cos(
-        yaw / 2)
-    qw = np.cos(roll / 2) * np.cos(pitch / 2) * np.cos(yaw / 2) + np.sin(roll / 2) * np.sin(pitch / 2) * np.sin(
-        yaw / 2)
+    # Desired correction action
+    roll_act = e_roll * self.p_roll + (e_roll - self.e_roll_prev) * self.d_roll
+    pitch_act = e_pitch * self.p_pitch + (e_pitch - self.e_pitch_prev) * self.d_pitch
+    yaw_act = e_yaw * self.p_yaw + (e_yaw - self.e_yaw_prev) * self.d_yaw
 
-    return [qw, qx, qy, qz]
+    self.e_roll_prev = e_roll
+    self.e_pitch_prev = e_pitch
+    self.e_yaw_prev = e_yaw
 
-for i in range(1000):
-    rnd_euler = np.random.randn(3)
-    rnd_quat = list(np.random.rand(4))
-    rnd_quat_normed = rnd_quat / np.linalg.norm(rnd_quat)
-    euler_my_res = q2e(rnd_quat_normed)
-    euler_lib_res = q.as_euler_angles(q.quaternion(*rnd_quat_normed))
-    quat_my_res = e2q(*q2e(rnd_quat_normed))
-    quat_lib_res = q.from_euler_angles(q.as_euler_angles(q.quaternion(*rnd_quat_normed))).components
-    #euler_my_res = q2e(e2q(*rnd_euler))
-    #euler_lib_res = q.as_euler_angles(q.from_euler_angles(*rnd_euler))
-    if not np.allclose(rnd_quat_normed, quat_my_res, rtol=0.001):
-         print(rnd_quat_normed, quat_my_res)
-    #if not np.allclose(euler_my_res, euler_lib_res, rtol=0.001):
-    #    print(euler_my_res, euler_lib_res)
+    m_1_act_total = + roll_act - pitch_act + yaw_act
+    m_2_act_total = - roll_act - pitch_act - yaw_act
+    m_3_act_total = + roll_act + pitch_act - yaw_act
+    m_4_act_total = - roll_act + pitch_act + yaw_act
+
+    # Translate desired correction actions to servo commands
+    m_1 = np.clip(t_throttle + m_1_act_total, 0, 1)
+    m_2 = np.clip(t_throttle + m_2_act_total, 0, 1)
+    m_3 = np.clip(t_throttle + m_3_act_total, 0, 1)
+    m_4 = np.clip(t_throttle + m_4_act_total, 0, 1)
+
+    # print([m_1, m_2, m_3, m_4])
+
+    if np.max([m_1, m_2, m_3, m_4]) > 1.1:
+        print("Warning: motor commands exceed 1.0. This signifies an error in the system", m_1, m_2, m_3, m_4,
+              t_throttle)
+
+    return m_1, m_2, m_3, m_4
