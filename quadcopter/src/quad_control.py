79573fc7dd313f1288e598c576b14afd5d7918d0
silverjoda <heizen.sh@gmail.com>
2020-12-03 15:57:38 +0000

    M

--- quadcopter/src/quad_control.py
+++ quadcopter/src/quad_control.py
@@ -276,11 +276,7 @@ class PWMDriver:
     def __init__(self, motors_on):
         self.motors_on = motors_on
 
-        if not self.motors_on:
-            print("Motors OFF, not initializing the PWMdriver. ")
-            return
-
-        self.pwm_freq = 50
+        self.pwm_freq = 100
         self.servo_ids = [0, 1, 2, 3]
 
         print("Initializing the PWMdriver. ")
@@ -297,20 +293,15 @@ class PWMDriver:
         :param vals: Throttle commands [0,1] corresponding to min and max values
         :return: None
         '''
-
+        
         if not self.motors_on:
-            print("Motors OFF, writing to servos")
-            return
+            vals = [0,0,0,0]
 
         for id in self.servo_ids:
             pulse_length = ((np.clip(vals[id], 0, 1) + 1) * 1000) / ((1000000. / self.pwm_freq) / 4096.)
             self.pwm.set_pwm(id, 0, int(pulse_length))
 
     def arm_escs(self):
-        if not self.motors_on:
-            print("Motors OFF, not arming motors")
-            return
-
         print("Setting escs to lowest value. ")
         self.write_servos([0, 0, 0, 0])
 
@@ -340,12 +331,12 @@ class Controller:
         print("Finished initializing the Controller")
 
     def setup_stabilization_control(self):
-        self.p_roll = 0.2
-        self.p_pitch = 0.2
+        self.p_roll = 0.4
+        self.p_pitch = 0.4
         self.p_yaw = 0.0
 
-        self.d_roll = 15.
-        self.d_pitch = 15.
+        self.d_roll = 1.4
+        self.d_pitch = 1.7
         self.d_yaw = 0.0
 
         self.e_roll_prev = 0
@@ -416,6 +407,8 @@ class Controller:
         '''
 
         print("Starting the control loop")
+        frame_ctr = 0
+        bundle_starttime = time.time()
         while True:
             iteration_starttime = time.time()
 
@@ -432,7 +425,7 @@ class Controller:
             velocity_targets = throttle, -t_roll, t_pitch, t_yaw
             pid_targets = throttle, t_roll, t_pitch, t_yaw
 
-            print(f"Pos: {position_rob}, pos_delta: {pos_delta}")
+            #print(f"Pos: {position_rob}, pos_delta: {pos_delta}")
 
             # Calculate stabilization actions
             if autonomous_control:
@@ -452,6 +445,12 @@ class Controller:
             self.PWMDriver.write_servos([m_1, m_2, m_3, m_4])
 
             while time.time() - iteration_starttime < self.config["update_period"]: pass
+
+            frame_ctr += 1
+
+            if frame_ctr % 100 == 0:
+                print(frame_ctr, time.time() - bundle_starttime)
+                bundle_starttime = time.time()
 
 
     def gather_data(self, n_iterations=20000):
