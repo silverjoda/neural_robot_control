f9504cadd9a94d4ab157fb9c4b1b237f349d18e9
Teymur Azayev <azayetey@fel.cvut.cz>
2021-02-08 11:29:50 +0100

    M

---
+++ hexapod/src/test_joystick.py
@@ -0,0 +1,60 @@
+import time
+import itertools
+import torch.nn as nn
+import torch.nn.functional as F
+import torch as T
+import numpy as np
+import os
+import logging
+
+import math
+import sys
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+import pygame
+from gyro import IMU
+import math as m
+import yaml
+import threading
+import quaternion
+
+class JoyController():
+    def __init__(self):
+        logging.info("Initializing joystick controller")
+        pygame.init()
+        self.joystick = pygame.joystick.Joystick(0)
+        self.joystick.init()
+        logging.info("Initialized gamepad: {}".format(self.joystick.get_name()))
+        logging.info("Finished initializing the joystick controller.")
+        self.button_x_state = 0
+
+    def get_joystick_input(self):
+        pygame.event.pump()
+        turn, vel = [self.joystick.get_axis(3), self.joystick.get_axis(1)]
+        print(turn, vel)
+        button_x = self.joystick.get_button(0)
+        pygame.event.clear()
+
+        turn = -turn / 2 # [-0.5, 0.5]
+        vel = np.maximum(vel * -1, 0)  # [0, 1]
+        print(f"Turn: {turn}, Vel: {vel}")
+
+        # button_x only when upon press
+        if self.button_x_state == 0 and button_x == 1:
+            self.button_x_state = 1
+            button_x = 1
+        elif self.button_x_state == 1 and button_x == 0:
+            self.button_x_state = 0
+            button_x = 0
+        elif self.button_x_state == 1 and button_x == 1:
+            self.button_x_state = 1
+            button_x = 0
+        else:
+            self.button_x_state = 0
+            button_x = 0
+
+        return turn, vel, button_x
+
+jc = JoyController()
+while True:
+    jc.get_joystick_input()
