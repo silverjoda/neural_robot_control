9f63c93bdf7d00ded0a88902fd61df77cdf5f023
Teymur Azayev <azayetey@fel.cvut.cz>
2021-02-26 16:37:17 +0100

    M

--- hexapod/src/hexapod_control_combined.py
+++ hexapod/src/hexapod_control_combined.py
@@ -9,7 +9,7 @@ import logging
 import smbus
 import math
 import sys
-logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+#logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 import pypot.dynamixel
 import pygame
 from gyro import IMU
@@ -19,7 +19,7 @@ import yaml
 import threading
 import quaternion
 # Torques are positive upwards and when leg is being pushed backward
-from stable_baselines3 import A2C, TD3
+from stable_baselines3 import TD3
 import RPi.GPIO as GPIO
     
 from peripherals import *
@@ -27,8 +27,8 @@ from peripherals import *
 class HexapodController:
     def __init__(self, config):
         self.config = config
-        self.max_servo_speed = self.config["max_servo_speed"] # [0:1023]
-        self.max_servo_torque = self.config["max_servo_torque"]  # [0:1023]
+        self.max_servo_speed = self.config["max_servo_speed"] # [0:1024]
+        self.max_servo_torque = self.config["max_servo_torque"]  # [0:1024]
 
         self.turn_transition_thresh = self.config["turn_transition_thresh"]
         self.angle_increment = self.config["angle_increment"]
@@ -38,7 +38,16 @@ class HexapodController:
         GPIO.setmode(GPIO.BCM)
         for ipt in self.leg_sensor_gpio_inputs:
             GPIO.setup(ipt, GPIO.IN)
- 
+
+        self.phases_op = np.array(
+            [-0.45378223061561584, 2.0218961238861084, 2.984712839126587, -1.3525876998901367, -1.7097233533859253,
+             1.027929663658142])
+        self.x_mult, self.y_offset, self.z_mult, self.z_offset = [
+            0.06,
+            0.12,
+            0.03,
+            -0.12]
+
         self.joints_rads_low = np.array(self.config["joints_rads_low"] * 6)
         self.joints_rads_high = np.array(self.config["joints_rads_high"] * 6)
         self.joints_rads_diff = self.joints_rads_high - self.joints_rads_low
@@ -55,13 +64,16 @@ class HexapodController:
         self.Ahrs = AHRS_RS()
         
         # Load policies
-        self.nn_policy_straight = TD3.load("agents/{}".format(self.config["policy_straight"]))
+        self.nn_policy_eef = TD3.load("agents/{}".format(self.config["policy_eef"]))
+        self.nn_policy_direct = TD3.load("agents/{}".format(self.config["policy_direct"]))
+
+        self.control_modes = ["eef", "direct"]
+        self.current_control_mode_idx = 0
 
         logging.info("Initializing robot hardware")
         self.init_hardware()
 
         self.observation_timestamp = time.time()
-        self.previous_joint_angles = [0] * 18
         self.angle = 0
         self.dynamic_time_feature = -1
         self.xd_queue = []
@@ -75,36 +87,45 @@ class HexapodController:
             iteration_starttime = time.time()
             # Read joystick
             turn, vel, button_x = self.joystick_controller.get_joystick_input()
-            # print(turn, vel, button_x)
+            #print(turn, vel, button_x)
 
             # Calculate discrete velocity level
-            self.angle_increment = vel * self.angle_increment
+            self.angle_increment = vel * self.config["angle_increment"]
+
+            if button_x:
+                self.current_control_mode_idx = not self.current_control_mode_idx
 
             # Idle
             if vel < 0.1 and abs(turn) < 0.1:
-                self.hex_write_ctrl([0, -0.5, 0.5] * 6)
+                self.hex_write_ctrl_direct([0, -0.5, 0.5] * 6)
                 self.Ahrs.reset_yaw()
-                self.dynamic_time_feature = -1.
-                print_sometimes("Idling",0.1)
+                self.dynamic_time_feature = -1
+                print_sometimes("Idling", 0.1)
                 time.sleep(0.1)
             else:
-                speed = dict(zip(self.ids, itertools.repeat(int(self.max_servo_speed * vel))))
-                self.dxl_io.set_moving_speed(speed)
-
                 # Read robot servos and hardware and turn into observation for nn
                 clipped_turn = np.clip(-turn * 3, -self.config["turn_clip_value"], self.config["turn_clip_value"])
-                policy_obs = self.hex_get_obs(clipped_turn)
+
+                if self.control_modes[self.current_control_mode_idx] == "direct":
+                    policy_obs = self.hex_get_obs_direct(clipped_turn)
+                else:
+                    policy_obs = self.hex_get_obs_eef(clipped_turn)
 
                 # Perform forward pass on nn policy
                 policy_act, _ = self.current_nn_policy.predict(policy_obs, deterministic=True)
 
-                # Calculate servo commands from policy action and write to servos
-                self.hex_write_ctrl(policy_act)
+                if self.control_modes[self.current_control_mode_idx] == "direct":
+                    speed = dict(zip(self.ids, itertools.repeat(int(self.max_servo_speed * vel))))
+                    self.dxl_io.set_moving_speed(speed)
+                    # Calculate servo commands from policy action and write to servos
+                    self.hex_write_ctrl_direct(policy_act)
+                else:
+                    # Calculate servo commands from policy action and write to servos
+                    self.hex_write_ctrl_eef(policy_act)
 
-                self.dynamic_time_feature = np.minimum(self.dynamic_time_feature + 0.003, 0)
+                self.dynamic_time_feature = np.minimum(self.dynamic_time_feature + 0.010, 1)
 
-            while (time.time() - iteration_starttime) < self.config["update_period"]: pass
-            print_sometimes(f"Actual iteration took {(time.time() - iteration_starttime)} s", 0.1)
+            while time.time() - iteration_starttime < self.config["update_period"]: pass
 
     def init_hardware(self):
         '''
@@ -152,7 +173,38 @@ class HexapodController:
             print("Turn angle: {}, roll: {}, pitch: {}, yaw: {}, quat: {}".format(turn, roll, pitch, yaw, quat))
             time.sleep(0.3)
 
-    def hex_get_obs(self, heading_spoof_angle=0):
+    def hex_get_obs_eef(self, heading_spoof_angle=0):
+        '''
+        Read robot hardware and return observation tensor for pytorch
+        :return:
+        '''
+
+        scrambled_ids = list(range(1,19))
+        np.random.shuffle(scrambled_ids)
+        scrambled_servo_positions = self.dxl_io.get_present_position(scrambled_ids)
+        servo_positions = [scrambled_servo_positions[scrambled_ids.index(i+1)] for i in range(18)]
+
+        # Reverse servo observations
+        servo_positions = np.array(servo_positions).astype(np.float32)
+        servo_positions[np.array([4,5,6,10,11,12,16,17,18])-1] = 1023 - servo_positions[np.array([4,5,6,10,11,12,16,17,18])-1]
+
+        # Read IMU (for now spoof perfect orientation)
+        roll, pitch, yaw, quat, vel_rob, timestamp = self.Ahrs.update(heading_spoof_angle=heading_spoof_angle)
+
+
+        # Turn servo positions into rads
+        joints_normed = (servo_positions / 1023) * 2 - 1
+        joint_angles_rads = joints_normed * 2.618
+
+        # Contacts
+        contacts = self.read_contacts()
+
+        # Make nn observation
+        obs = np.concatenate((quat, vel_rob, [yaw], joint_angles_rads, contacts, [self.dynamic_time_feature]))
+
+        return obs
+
+    def hex_get_obs_direct(self, heading_spoof_angle=0):
         '''
         Read robot hardware and return observation tensor for pytorch
         :return:
@@ -202,19 +254,61 @@ class HexapodController:
 
         return obs
 
-    def hex_write_ctrl(self, nn_act):
+    def hex_write_ctrl_eef(self, nn_act):
         '''
         Turn policy action tensor into servo commands and write them to servos
         :return: None
         '''
-    
+
+        ctrl_raw = np.tanh(nn_act)
+
+        x_mult_arr = [self.x_mult + np.tanh(ctrl_raw[7]) * self.config["x_mult_scalar"],
+                      self.x_mult + np.tanh(ctrl_raw[8]) * self.config["x_mult_scalar"]] * 3
+
+        targets = []
+        for i in range(6):
+            target_x = np.cos(-self.angle * 2 * np.pi + self.phases_op[i]) * x_mult_arr[i]
+            target_y = self.y_offset
+            target_z = np.clip(
+                np.sin(-self.angle * 2 * np.pi + self.phases_op[i]) * self.z_mult + self.z_offset + np.tanh(
+                    ctrl_raw[8 + i]) * self.config["z_aux_scalar"], -0.13, -0.04)
+            targets.append([target_x, target_y, target_z])
+
+        joint_angles = self.my_ikt(targets, self.y_offset)
+        joint_angles_normed = np.clip(np.array(joint_angles) / 2.618, -1, 1) # [-1,1] corresponding to bounds on servos
+        joint_angles_servo = (joint_angles_normed * 0.5 + 0.5) * 1023
+
+        self.angle += self.angle_increment
+
+        scaled_act = np.array([np.asscalar(joint_angles_servo[i]) for i in range(18)]).astype(np.uint16)
+
+        # Reverse servo signs for right hand servos (This part is retarded and will need to be fixed)
+        scaled_act[np.array([4,5,6,10,11,12,16,17,18])-1] = 1023 - scaled_act[np.array([4,5,6,10,11,12,16,17,18])-1]
+
+        scrambled_ids = list(range(1, 19))
+        np.random.shuffle(scrambled_ids)
+        scrambled_acts = [scaled_act[si - 1] for si in scrambled_ids]
+        scaled_act_dict = dict(zip(scrambled_ids, scrambled_acts))
+
+        if self.config["motors_on"]:
+            self.dxl_io.set_goal_position(scaled_act_dict)
+
+    def hex_write_ctrl_direct(self, nn_act):
+        '''
+        Turn policy action tensor into servo commands and write them to servos
+        :return: None
+        '''
+
         nn_act_clipped = np.tanh(nn_act)
 
         # Map [-1,1] to correct 10 bit servo value, respecting the scaling limits imposed during training
-        scaled_act = np.array([(np.asscalar(nn_act_clipped[i]) * 0.5 + 0.5) * self.joints_10bit_diff[i] + self.joints_10bit_low[i] for i in range(18)]).astype(np.uint16)
+        scaled_act = np.array(
+            [(np.asscalar(nn_act_clipped[i]) * 0.5 + 0.5) * self.joints_10bit_diff[i] + self.joints_10bit_low[i] for i
+             in range(18)]).astype(np.uint16)
 
-        # Reverse servo signs for right hand servos (Thsi part is retarded and will need to be fixed)
-        scaled_act[np.array([4,5,6,10,11,12,16,17,18])-1] = 1023 - scaled_act[np.array([4,5,6,10,11,12,16,17,18])-1]
+        # Reverse servo signs for right hand servos (This part is retarded and will need to be fixed)
+        scaled_act[np.array([4, 5, 6, 10, 11, 12, 16, 17, 18]) - 1] = 1023 - scaled_act[
+            np.array([4, 5, 6, 10, 11, 12, 16, 17, 18]) - 1]
 
         scrambled_ids = list(range(1, 19))
         np.random.shuffle(scrambled_ids)
@@ -247,35 +341,44 @@ class HexapodController:
         normalized_torques_corrected = normalized_torques * torque_dirs_corrected
         return normalized_torques_corrected
 
+    def my_ikt(self, target_positions, y_offset):
+        rotation_angles = [np.pi / 4, np.pi / 4, 0, 0, -np.pi / 4, -np.pi / 4]
+        joint_angles = []
+        for i, tp in enumerate(target_positions):
+            tp_rotated = self.rotate_eef_pos(tp, rotation_angles[i], y_offset)
+            joint_angles.extend(self.single_leg_ikt(tp_rotated))
+        return joint_angles
 
-    def test_leg_coordination(self):
-        '''
-        Perform leg test to determine correct mapping and range
-        :return:
-        '''
+    def rotate_eef_pos(self, eef_xyz, angle, y_offset):
+        return [eef_xyz[0] * np.cos(angle), eef_xyz[0] * np.sin(angle) + y_offset, eef_xyz[2]]
 
-        if not self.config["motors_on"]:
-            print("Motors are off, not performing test")
+    def single_leg_ikt(self, eef_xyz):
+        x,y,z = eef_xyz
 
-        logging.info("Starting leg coordination test")
-        VERBOSE = True
-        sc = 1.0
-        test_acts = [[0, 0, 0], [0, sc, sc], [0, -sc, -sc], [0, sc, -sc], [0, -sc, sc], [sc, 0, 0], [-sc, 0, 0]]
-        for i, a in enumerate(test_acts):
-            self.hex_write_ctrl(np.array(a * 6))
-            time.sleep(3)
-            joints_normed = self.hex_get_obs()[0].detach().numpy()[:18] # rads
-            joints_rads = (((joints_normed + 1) / 2) * self.joints_rads_diff) + self.joints_rads_low 
-            act_rads = (np.array(a * 6) * 0.5 + 0.5) * self.joints_rads_diff + self.joints_rads_low
-            
-            if VERBOSE:
-                print("Obs rads: ", joints_rads) 
-                print("Obs norm: ", joints_normed) 
-                print("For action rads: ", act_rads) 
-                print("action norm: ", a) 
-            
-        logging.info("Finished leg coordination test, exiting")
-        quit()
+        assert -0.15 < x < 0.15
+        assert 0.05 < y < 0.3
+        assert -0.2 < z < 0.2
+
+        q1 = 0.2137
+        q2 = 0.785
+
+        C = 0.052
+        F = 0.0675
+        T = 0.132
+
+        psi = (np.arcsin(x/y))
+        Cx = C * np.sin(psi)
+        Cy = C * np.cos(psi)
+        R = np.sqrt((x-Cx)**2 + (y-Cy)**2 + (z)**2)
+        alpha = np.arcsin(-z/R)
+
+        a = np.arccos((F**2 + R**2 - T**2) / (2 * F * R))
+        b = np.arccos((F ** 2 + T ** 2 - R ** 2) / (2 * F * T))
+
+        th1 = alpha - q1 - a
+        th2 = np.pi - q2 - b
+
+        return -psi, th1, th2
 
     def test_AHRS_RS(self):
         while True:
